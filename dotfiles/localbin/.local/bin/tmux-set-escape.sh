b8eecad27a058eb8130c7cf5eab40c5e6377d2a6
Andrew Malcolmson <andmalc@gmail.com>
2016-12-07 18:44:14 -0500

    updates

--- dotfiles/localbin/.local/bin/tmux-set-escape.sh
+++ dotfiles/localbin/.local/bin/tmux-set-escape.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [[ -n $SSH_TTY ]];then
-    tmux unbind-key C-b;
-    tmux set -g prefix `;
-    tmux bind ` send-prefix;
-    tmux set -g status-bg colour12; #blue
+    tmux unbind-key C-b
+    tmux  set -g prefix \`
+    tmux bind \` send-prefix
+    tmux set -g status-bg colour12
 else
     tmux display-message "Not a ssh session";
 fi
