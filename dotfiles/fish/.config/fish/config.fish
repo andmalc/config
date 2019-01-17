7b3a24804568f48b5a6af547839caceaad90df35
Andrew Malcolmson <andmalc@gmail.com>
2019-01-17 13:42:17 -0500

    pymux and fish on TP

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -4,3 +4,28 @@ set -gx CDPATH $CDPATH .  ~
 
 # Neovim
 set -gx NVIM_LISTEN_ADDRESS /tmp/nvimsocket
+
+if not set -q abbrs_initialized
+  set -U abbrs_initialized
+  echo -n Setting abbreviations... 
+
+  abbr g 'git'
+  abbr ga 'git add'
+  abbr gb 'git branch'
+  abbr gbl 'git blame'
+  abbr gc 'git commit -m'
+  abbr gco 'git checkout'
+  abbr gcp 'git cherry-pick'
+  abbr gd 'git diff'
+  abbr gf 'git fetch'
+  abbr gl 'git log'
+  abbr gm 'git merge'
+  abbr gp 'git push'
+  abbr gpl 'git pull'
+  abbr gr 'git remote'
+  abbr gs 'git status'
+  abbr gst 'git stash'
+
+  echo 'Done'
+end
+
