b4e1487927384fb78d8bb9ee8968c0602acf8962
Andrew Malcolmson <andmalc@gmail.com>
2021-04-10 20:21:33 +0000

    fish file cleanup

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -2,20 +2,20 @@
 
 set -gx CDPATH $CDPATH .  ~
 
-set -g fisher_path ~/config/dotfiles/fish/.config/fish/fisher
-set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
-set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+#set -g fisher_path ~/config/dotfiles/fish/.config/fish/fisher
+#set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+#set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
 
-for file in $fisher_path/conf.d/*.fish
+#for file in $fisher_path/conf.d/*.fish
 #    builtin source $file 2> /dev/null
-end
+#end
 
 # length of directory abbreviations in prompt
 set fish_prompt_pwd_dir_length 3
 
 if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
+    #    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    #source "$BASE16_SHELL/profile_helper.fish"
 end
 
 # Neovim
@@ -23,35 +23,17 @@ set -gx NVIM_LISTEN_ADDRESS /tmp/nvimsocket
 
 
 
-if not set -q abbrs_initialized
-  set -U abbrs_initialized
-  echo -n Setting abbreviations... 
-
-  abbr g 'git'
-  abbr ga 'git add'
-  abbr gb 'git branch'
-  abbr gbl 'git blame'
-  abbr gc 'git commit -m'
-  abbr gco 'git checkout'
-  abbr gcp 'git cherry-pick'
-  abbr gd 'git diff'
-  abbr gf 'git fetch'
-  abbr gl 'git log'
-  abbr gm 'git merge'
-  abbr gp 'git push'
-  abbr gpl 'git pull'
-  abbr gr 'git remote'
-  abbr gs 'git status'
-  abbr gst 'git stash'
-
-  echo 'Done'
+if status --is-interactive
+  abbr --add --global gco git checkout
+  abbr --add --global g 'git'
+  abbr --add --global ga 'git add'
+  abbr --add --global gb 'git branch'
+  abbr --add --global gbl 'git blame'
+  abbr --add --global gc 'git commit -m'
+  abbr --add --global gco 'git checkout'
+  abbr --add --global gd 'git diff'
+  abbr --add --global gp 'git push'
+  abbr --add --global gpl 'git pull'
+  abbr --add --global gs 'git status'
 end
 
-# https://github.com/arumoy-shome/dotfiles/blob/master/fish/functions/g.fish
-function g
-    if test "$argv"
-        command git $argv
-    else
-        command git status
-    end
-end
